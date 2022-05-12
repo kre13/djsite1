9ffc9d2d9df7e6ddf84408140965133600eaca3b
dbzomb <dbzomb@gmail.com>
2022-05-12 15:39:31 +0300

    Initial Commit

---
+++ coolsite/new/views.py
@@ -0,0 +1,36 @@
+from django.http import HttpResponse, HttpResponseNotFound
+from django.shortcuts import render
+from .models import *
+
+menu = [{'title': "О сайте", 'url_name': 'about'},
+        {'title': "Добавить статью", 'url_name': 'add_page'},
+        {'title': "Обратная связь", 'url_name': 'contact'},
+        {'title': "Войти", 'url_name': 'login'},
+]
+def index(request):
+    posts = New.objects.all()
+    context = {
+        'posts': posts,
+        'menu': menu,
+        'title': 'Главная страница'
+    }
+    return render(request, 'new/index.html', context=context)
+
+def about(request):
+    return render(request, 'new/about.html', {'menu': menu,'title': 'О сайте'})
+
+def addpage(request):
+    return HttpResponse("Добавление статьи")
+
+def contact(request):
+    return HttpResponse("Обратная связь")
+
+def login(request):
+    return HttpResponse("Авторизация")
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound("<h1>Страница не найдена</h1>")
+
+def show_post(request, post_id):
+    return HttpResponse(f"Отображение статьи с id = {post_id}")
+
